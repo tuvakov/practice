9da063ac56658d64c1b47d4824b5655af0cd8216
Jemshit Tuvakov <jemshit.tuvakov@gmail.com>
2022-12-17 02:07:28 +0900

    Add TopKFrequentWords

---
+++ src/main/kotlin/leetcode/string/TopKFrequentWords.kt
@@ -0,0 +1,35 @@
+package leetcode.string
+
+import java.util.Comparator
+import java.util.PriorityQueue
+
+/*
+ * https://leetcode.com/problems/top-k-frequent-words/
+ */
+
+class TopKFrequentWords {
+
+    fun topKFrequent(words: Array<String>, k: Int): List<String> {
+
+        val map = words.groupingBy { it }.eachCount()
+
+        val comparator = Comparator<Pair<String, Int>> { o1, o2 ->
+            val r = o2.second.compareTo(o1.second)
+            if (r == 0) o1.first.compareTo(o2.first) else r
+        }
+
+        val heap = PriorityQueue<Pair<String, Int>>(comparator)
+
+        map.forEach { (t, u) -> heap.add(Pair(t, u)) }
+
+        val result = mutableListOf<String>()
+        var counter = 0
+
+        while (counter < k) {
+            result.add(heap.poll().first)
+            counter++
+        }
+
+        return result
+    }
+}
---
+++ src/test/kotlin/leetcode/string/TopKFrequentWordsTest.kt
@@ -0,0 +1,41 @@
+package leetcode.string
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class TopKFrequentWordsTest {
+
+    private val subject = TopKFrequentWords()
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    fun topKFrequent(words: Array<String>, k: Int, output: List<String>) {
+        val result = subject.topKFrequent(words, k)
+        assertEquals(output, result)
+    }
+
+    companion object {
+
+        @JvmStatic
+        private fun arguments(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    arrayOf("i", "love", "leetcode", "i", "love", "coding"),
+                    2,
+                    listOf("i", "love")
+                ),
+                Arguments.of(
+                    arrayOf(
+                        "the", "day", "is", "sunny", "the", "the",
+                        "the", "sunny", "is", "is"
+                    ),
+                    4,
+                    listOf("the", "is", "sunny", "day")
+                )
+            )
+        }
+    }
+}
